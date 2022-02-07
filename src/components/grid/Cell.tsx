306c6840dae13a95426252da4fac279f75592346
Abrar Hayat <abrarhayat@gmail.com>
2022-02-08 00:21:26 +0600

    Added enhancements from base project

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -1,12 +1,26 @@
 import { CharStatus } from '../../lib/statuses'
 import classnames from 'classnames'
+import { REVEAL_TIME_MS } from '../../constants/settings'
 
 type Props = {
   value?: string
   status?: CharStatus
+  isRevealing?: boolean
+  isCompleted?: boolean
+  position?: number
 }
 
-export const Cell = ({ value, status }: Props) => {
+export const Cell = ({
+  value,
+  status,
+  isRevealing,
+  isCompleted,
+  position = 0,
+}: Props) => {
+  const isFilled = value && !isCompleted
+  const shouldReveal = isRevealing && isCompleted
+  const animationDelay = `${position * REVEAL_TIME_MS}ms`
+
   const classes = classnames(
     'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
@@ -18,9 +32,16 @@ export const Cell = ({ value, status }: Props) => {
       'shadowed bg-lime-500 text-white border-lime-500': status === 'correct',
       'shadowed bg-yellow-500 dark:bg-lightyellow-700 text-white border-yellow-500 dark:border-lightyellow-700':
         status === 'present',
-      'cell-animation': !!value,
+      'cell-fill-animation': isFilled,
+      'cell-reveal': shouldReveal,
     }
   )
 
-  return <div className={classes}>{value}</div>
+  return (
+    <div className={classes} style={{ animationDelay }}>
+      <div className="letter-container" style={{ animationDelay }}>
+        {value}
+      </div>
+    </div>
+  )
 }
